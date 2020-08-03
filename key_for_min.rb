7c10574b92033ffa78de2d4edadb6426861427bb
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-03 06:12:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,15 @@ def key_for_min_value(name_hash)
   small_value = nil 
   name_hash.each do |key, value|
     if small_value == nil
-      small_value = 
-  end
-end
+      small_value = value 
+            small_key = key
+        elsif
+            value < small_value
+            small_value = value 
+            small_key = key  
+        end 
+    end 
+    smallest_key
 
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 key_for_min_value(ikea)
