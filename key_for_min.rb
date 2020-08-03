e91d7b20d2815c5182d84762f50e862d9e4eebc7
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-03 06:11:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   small_key = nil 
   small_value = nil 
   name_hash.each do |key, value|
-    if value
+    if small_value == nil
+      small_value = 
   end
 end
 
